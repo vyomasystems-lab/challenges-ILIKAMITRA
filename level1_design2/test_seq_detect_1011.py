0ba6b95315069a58b25d0a2b29d69293b19e53aa
ILIKA MITRA <aminoacids1900@gmail.com>
2022-08-01 15:54:27 +0000

    submission2

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -9,7 +9,15 @@ from pathlib import Path
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
-
+def seq_detected(input_1):
+    exp=[]
+    det = [i for i in range(len(input_1)) if input_1.startswith('1011', i)]
+    detected_places=[i+len('1011')-1 for i in det]
+    exp=[0 for i in range(len(input_1))]
+    #print(detected_places)
+    for i in detected_places:
+            exp[i]=1
+    return exp
 @cocotb.test()
 async def test_seq_bug1(dut):
     """Test for seq detection """
@@ -24,3 +32,14 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
+    input_1='101011'
+    ex=seq_detected(input_1)
+    inp_arr= [int(i) for i in list(input_1)]
+    output =[]
+    for i in range(len(inp_arr)):
+        dut.inp_bit.value= inp_arr[i]
+        await FallingEdge(dut.clk)
+        output.append(dut.seq_seen.value)
+    print("Dut ", output)
+    print("exp ", ex)
+    assert output== ex, "ERROR IN DETECTING expected {e} but got output as {d}".format(e=ex, d=output)
